d6af9183920bdedcf3b3c98c3fbe5bc5634ae859
Julien Palmas <julien.palmas@gmail.com>
2010-09-24 20:36:21 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Compta::Application.config.secret_token = '1b2e0809227e89534fabcd5b495252f84937207a4638f5bd6f5addc63f7ca0434b51400728265b6c50d2884118825d6d448c2b0a615c2e971188da6747592d62'
