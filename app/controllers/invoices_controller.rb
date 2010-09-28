8a48ba6960bfa3684414d1896fa1c25b3c7ac6e5
Julien Palmas <julien.palmas@gmail.com>
2010-09-28 11:24:36 +0200

    refactored invoices index

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -9,19 +9,10 @@ class InvoicesController < ApplicationController
   #########
   
   def index
-    @client = Client.find_by_id params[:client_id]
-    @contract = Contract.find_by_id params[:contract_id]
-    
-    scope = if @client
-      @client.invoices
-    elsif @contract
-      @contract.invoices
-    else
-      Invoice
-    end
-    
-    scope = scope.where(:invoiced_on => (Date.parse(params[:from])..Date.parse(params[:to]))) if params[:from]
-    @invoices = scope.includes(:client, :contract)
+    @invoices = Invoice.includes(:client, :contract)
+    @invoices = @invoices & Client.where(:id => params[:client_id]) if params[:client_id]
+    @invoices = @invoices & Contract.where(:id => params[:contract_id]) if params[:contract_id]
+    @invoices = @invoices.where(:invoiced_on => (Date.parse(params[:from])..Date.parse(params[:to]))) if params[:from]
   end
   
   def new
