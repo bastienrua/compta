1440a9bf0f3320b16ad23cfd526d80a6a3db802e
Julien Palmas <julien.palmas@gmail.com>
2010-10-26 01:46:26 +0200

    new invoice_line_contract_link helper

--- app/helpers/invoice_lines_helper.rb
+++ app/helpers/invoice_lines_helper.rb
@@ -1,2 +1,8 @@
 module InvoiceLinesHelper
+  
+  def invoice_line_contract_link(line)
+    return unless line.contract
+    link_to line.contract.title, edit_contract_path(line.contract)
+  end
+  
 end
