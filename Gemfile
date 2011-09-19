ef07703697dc9f183f86de44a060b20e7f07a911
bastienrua <bastien.rua34@gmail.com>
2011-09-19 18:43:44 +0200

    added javascript datepicker for invoices form

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem "haml", "~> 3.0.18"
 gem "formtastic", "~> 1.1.0"
 gem "compass", "~> 0.10.5"
 gem "pg", "~> 0.9.0"
-
+gem "sqlite3"
 group :development do
 #  gem "rspec-rails", "~> 2.0.1"
   gem 'sqlite3-ruby', :require => 'sqlite3'
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,19 +14,19 @@
 ActiveRecord::Schema.define(:version => 20101025223912) do
 
   create_table "clients", :force => true do |t|
-    t.string    "name"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "contracts", :force => true do |t|
-    t.integer   "client_id"
-    t.decimal   "amount_excl_vat"
-    t.decimal   "vat"
-    t.date      "issued_on"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.string    "title"
+    t.integer  "client_id"
+    t.decimal  "amount_excl_vat"
+    t.decimal  "vat"
+    t.date     "issued_on"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "title"
   end
 
   add_index "contracts", ["client_id"], :name => "index_contracts_on_client_id"
