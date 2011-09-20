cc7591cd463dd6f4e06d3ed737ef2b8b59d24322
bastienrua <bastien.rua34@gmail.com>
2011-09-20 16:51:37 +0200

    automatically set invoice number in new action

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -17,6 +17,27 @@ class InvoicesController < ApplicationController
   
   def new
     @invoice = Invoice.new
+    if !Invoice.all.empty?
+      if Invoice.last.number[1,2] == Date.current.year.to_s[2,3]
+        number = Invoice.last.number.gsub("F#{Date.current.year.to_s[2,3]}", "").to_i
+        number += 1
+        
+        last_number = case number
+        when (0...10)
+          last_number = "00#{number}"
+        when (10...100)
+          last_number = "0#{number}"
+        else
+          number.to_s
+        end
+        
+        @invoice.number = "F" + Date.current.year.to_s[2,3] + last_number
+      else
+        @invoice.number = "F" + Date.current.year.to_s[2,3] + "001"
+      end
+    else
+      @invoice.number = "F" + Date.current.year.to_s[2,3] + "001"
+    end
   end
   
   def edit
