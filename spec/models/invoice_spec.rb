8881459462968f02312a714db7a0dfce843a605c
bastienrua <bastien.rua34@gmail.com>
2011-09-21 15:53:44 +0200

    refactored invoice_spec

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,38 +1,35 @@
 require 'spec_helper'
 
 describe Invoice do
-  it "should fail to save invoice without date" do
-    a = Invoice.new
-    a.should have(1).error_on(:invoiced_on)
+  
+  subject {FactoryGirl.build(:invoice)}
+  
+  its(:number) { should == "F11001" }
+  
+  context "when one invoice was created last year" do
+    before { FactoryGirl.create(:invoice, :number => "F10001") }
+  
+    its(:number) { should == "F11001" }
   end
   
-  it "should have a invoice number = F11001 (first invoice of the year)" do
-    a = Invoice.new
-    a.number = "F10001"
-    a.invoiced_on = Date.today.prev_year
-    a.save
+  context "when 2 invoices exist for the same year" do
     
-    f = Invoice.new
-    f.number.should == "F11001"
+    before do
+      
+      ["F11001", "F11002"].each do |e|
+        FactoryGirl.create(:invoice, :number => e)
+      end
+      
+    end
+    
+    its(:number) { should == "F11003" }
+  
   end
   
-  it "should have a invoice number = F11003 (third invoice of the year)" do
-    a = Invoice.new
-    a.number = "F11001"
-    a.invoiced_on = Date.today.prev_year
-    a.save
-    b = Invoice.new
-    b.number = "F11002"
-    b.invoiced_on = Date.today.prev_year
-    b.save
-    
-    f = Invoice.new
-    f.number.should == "F11003"
+  describe "invoiced_on" do
+    it "should be present" do
+      Invoice.new.should have(1).error_on(:invoiced_on)
+    end
   end
-  let(:number) {"F#{Date.today.year.to_s[2,3]}001"}
-  it "should create an invoice with number = F#{Date.today.year.to_s[2,3]}001 (empty invoices table)" do
-    f = Invoice.new
-    f.number.should == "F#{Date.today.year.to_s[2,3]}001"
-  end
-
+  
 end
